aa272f98a6a765f8f20e354f7e37d6e32f7d5fd5
Justin A Oeters <justin.oeters@gmail.com>
2023-04-25 20:21:45 -0400

    randomQuoteMachineComplete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+const [quoteData, setQuoteData] = useState({})
+
+useEffect(() => {
+getQuote()
+}, []);
+
+const getQuote = ( ) => {
+  fetch('https://api.quotable.io/random')
+  .then((response) => {return response.json();})
+  .then((data) => {
+    setQuoteData({
+      text: data.content,
+      author: data.author
+    })
+  })
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <p id="text">{quoteData.text}</p>
+        <p id="author">{quoteData.author}</p>
+        <button id="new-quote" onClick={getQuote}>New Quote</button>
+        <a class="button" id="tweet-quote" target="_top" href="https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text=%22Start%20where%20you%20are.%20Use%20what%20you%20have.%20%20Do%20what%20you%20can.%22%20Arthur%20Ashe">
+            <i class="fa fa-twitter"></i> 
+          </a>
+      </div>
     </div>
   );
 }
